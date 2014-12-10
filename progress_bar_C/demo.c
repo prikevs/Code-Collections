69e31ea3ae78bb852507f705c6030ac7633bfd5b
PrKevince <wildfire_ks@foxmail.com>
2014-12-10 18:24:18 +0800

    progress bar added

---
+++ progress_bar_C/demo.c
@@ -0,0 +1,27 @@
+/**
+ * program bar test program.
+ *
+ * @author chenxin <chenxin619315@gmail.com>
+ */
+#include "progress.h"
+#include <unistd.h>
+ 
+int main(int argc, char *argv[] )
+{
+    progress_t bar;
+    //progress_init(&bar, "", 50, PROGRESS_NUM_STYLE);
+    progress_init(&bar, "encrypting", 50, PROGRESS_BGC_STYLE);
+    //progress_init(&bar, "", 50, PROGRESS_BGC_STYLE);
+ 
+    int i;
+    for ( i = 0; i <= 50; i++ ) {
+    progress_show(&bar, i/50.0f);
+    sleep(1);
+    }
+    printf("\n+-Done\n");
+ 
+    progress_destroy(&bar);
+ 
+    return 0;
+}
+
